fbb432f7ff270a4d61d2386667cf0b880eededc0
isralr <ilaramac@myuax.com>
2024-10-24 18:43:34 +0200

    añadido parcial á, é, í, ó, ú

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "parte1/bandera_error.h"
 #include <locale>
+#include <codecvt>
 #include <stdexcept>
 #include "parte2/ejercicio2.h"
 #include "parte3/ejercicio3.h"
@@ -13,7 +14,7 @@ int main() {
 
     // Se verifica regularmente la bandera de error
     if (errorFlag) {
-        std::cout << "Ocurrió un error: división por cero." << std::endl;
+        std::wcout << L"Ocurrió un error: división por cero." << std::endl;
         // Se maneja el error y se reinicia la bandera
         errorFlag = false;
     }
@@ -21,7 +22,7 @@ int main() {
     // Uso de la versión que devuelve un valor de error
     int resultError = divideError(5, 0);
     if (resultError == -1) {
-        std::cout << "Error: división por cero." << std::endl;
+        std::wcout << L"Error: división por cero." << std::endl;
     }
 
     // Uso de la versión que lanza una excepción
@@ -29,14 +30,14 @@ int main() {
         int resultException = divideException(5, 0);
     }
     catch (const std::invalid_argument& e) {
-        std::cout << "Excepción: " << e.what() << std::endl;
+        std::wcout << L"Excepción: " << e.what() << std::endl;
     }
 
     try {
         func1();
     }
     catch (const std::runtime_error& e) {
-        std::cout << "Excepción capturada en main: " << e.what() << std::endl;
+        std::wcout << L"Excepción capturada en main: " << e.what() << std::endl;
     }
 
     return 0;
